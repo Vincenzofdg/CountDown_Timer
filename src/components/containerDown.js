bb60a71469fedb87f61de5a308d636836e0d8f54
Vincenzofdg <vincenzofdg.dev@gmail.com>
2022-10-23 03:53:49 -0300

    feat: setInterval does not work =/

--- src/components/containerDown.js
+++ src/components/containerDown.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, View, Pressable, Text} from 'react-native';
 
+import Context from '../Context/context';
+
 function ContainerDown() {
+  const {hour, min, sec, setHour, setMin, setSec} = useContext(Context);
+
+  // const timer = () => {
+  //   if (sec === 0) {
+  //     if (min === 0) {
+  //       setHour(hour - 1);
+  //       setMin(60);
+  //     }
+  //     setMin(min - 1);
+  //     setSec('60');
+  //   }
+  //   setSec(sec - 1);
+  // };
+
+  const action = () => {
+    setInterval(() => {
+      if (sec === '0') {
+        if (hour === '0' && min === '0') {
+          setSec('0');
+          return;
+        }
+        if (hour === '0' && min !== '0') {
+          setMin(`${min - 1}`);
+          setSec('60');
+          return;
+        }
+        if (hour !== '0' && min === '0') {
+          setHour(`${hour - 1}`);
+          setMin('59');
+          setSec('60');
+          return;
+        }
+        if (hour !== '0' && min !== '0') {
+          setMin(`${min - 1}`);
+          setSec('60');
+          return;
+        }
+      }
+      setSec(`${sec - 1}`);
+    }, 1000);
+  };
+
   return (
     <View style={styles.display}>
-      <Pressable style={styles.btn}>
+      <Pressable style={styles.btn} onPressOut={() => action()}>
         <Text style={styles.text}>Start</Text>
       </Pressable>
     </View>
@@ -36,3 +80,30 @@ const styles = StyleSheet.create({
 });
 
 export default ContainerDown;
+
+// const action = () => {
+//   // setInterval(timer, 1000);
+//   if (sec === '0') {
+//     if (hour === '0' && min === '0') {
+//       setSec('0');
+//       return;
+//     }
+//     if (hour === '0' && min !== '0') {
+//       setMin(`${min - 1}`);
+//       setSec('60');
+//       return;
+//     }
+//     if (hour !== '0' && min === '0') {
+//       setHour(`${hour - 1}`);
+//       setMin('59');
+//       setSec('60');
+//       return;
+//     }
+//     if (hour !== '0' && min !== '0') {
+//       setMin(`${min - 1}`);
+//       setSec('60');
+//       return;
+//     }
+//   }
+//   setSec(`${sec - 1}`);
+// };
--- src/components/elements/timeInput.js
+++ src/components/elements/timeInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, View, Text, TextInput} from 'react-native';
 
-function Input({num, tag, setter}) {
+function Timer({num, tag, setter}) {
   const ruleTag = t => {
     switch (t) {
       case 'h':
@@ -18,13 +18,12 @@ function Input({num, tag, setter}) {
 
   const handleChange = p => {
     const rule = ruleTag(tag);
-
     if (p >= rule) {
       setter(0);
       return;
     }
 
-    setter(p);
+    setter(Number(p));
   };
 
   return (
@@ -57,27 +56,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Input;
-
-// const handleChange = p => {
-//   const array = p.split('');
-//   console.log(array);
-//   const oldValue = array.shift();
-//   const newValue = array.pop();
-
-//   if (oldValue === '1' || oldValue === '2') {
-//     const arrayResult = [oldValue, newValue];
-//     const result = arrayResult.join('');
-
-//     if (Number(result) >= 24) {
-//       setter('0' + 'h');
-//       return;
-//     }
-
-//     setter(oldValue + newValue + 'h');
-//     return;
-//   }
-
-//   setter(newValue + 'h');
-//   console.log(oldValue);
-// };
+export default Timer;
